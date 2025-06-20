896f4a98b2332e972c026604058863467db1493d
Subhasis Nayak <subhasishnayak88@gmail.com>
2025-06-20 12:03:54 +0530

    Update production URLs to Vercel deployment

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://your-vercel-url.vercel.app/"; // This will be updated after Vercel deployment
+const urlProd = "https://mail-magic.vercel.app/"; // Updated with actual Vercel URL
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
